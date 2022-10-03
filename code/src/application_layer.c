9fa5b6edcef538c504a5b25bd8b7814d4cc61b5a
Daniela Tomás <daniela.stomas02@gmail.com>
2022-10-03 16:12:49 +0100

    update application_layer.c

--- code/src/application_layer.c
+++ code/src/application_layer.c
@@ -1,9 +1,29 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
+#include "string.h"
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
-                      int nTries, int timeout, const char *filename)
-{
-    // TODO
+                      int nTries, int timeout, const char *filename) {
+
+  LinkLayer connectionParameters;
+  strcpy(connectionParameters.serialPort,serialPort);
+
+  if(strcmp(role,"rx") == 0) {
+    connectionParameters.role = LlRx;
+  }
+  else if(strcmp(role,"tx") == 0) {
+    connectionParameters.role = LlTx;
+  }
+  else {
+      perror(role);
+  }
+  
+  connectionParameters.baudRate = baudRate;
+  connectionParameters.nRetransmissions = nTries;
+  connectionParameters.timeout = timeout;
+
+  llopen(connectionParameters);
 }
+
